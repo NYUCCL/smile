4afa6c1729bc4f10d82588c97b59584e15343048
markkho <markkho@users.noreply.github.com>
2026-04-09 16:54:30 -0400

    Write custom readme; update get_secrets

--- scripts/get_secrets.sh
+++ scripts/get_secrets.sh
@@ -10,4 +10,8 @@ for file in .env.deploy.local .env.docs.local .env.local; do
   echo "Downloaded: $file"
 done
 
+echo "Fetching service account key..."
+gh api "repos/$REPO/contents/.service-account-key.json" --jq '.content' | base64 -d > "firebase/.service-account-key.json"
+echo "Downloaded: .service-account-key.json"
+
 echo "Done."
